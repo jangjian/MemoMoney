52aca7a7adb45fdeb8b9eec8041b0cfdd4f7806e
Kim Jiwoo <88700207+jiwoo1087@users.noreply.github.com>
2023-11-07 19:49:23 +0900

    텍스트 입력 받기 및 배경 수정

--- MemoMoney/src/MemoMoney/Memo.java
+++ MemoMoney/src/MemoMoney/Memo.java
@@ -14,15 +14,50 @@ public class Memo extends JFrame {
             super.paintComponent(g);
         }
     };
-    // km
+    
+    private JTextArea[] textAreas = new JTextArea[4];
+    private JScrollPane[] scrollPanes = new JScrollPane[4];
+   
     Memo() {
         setTitle("MemoMoney");
 
         JTextArea textArea = new JTextArea();
         JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.setBounds(20,30,600,700);
+        Font font = new Font("나눔손글씨 장미체", Font.PLAIN, 30);
+        textArea.setFont(font);
+
+        
+        // 레이아웃 매니저 설정
+        p1.setLayout(null);
         p1.add(scrollPane);
+        
+        scrollPane.setBounds(45,185,505,480);
+        scrollPane.setOpaque(false);
+        scrollPane.getViewport().setOpaque(false);
+        scrollPane.setBorder(null);
+        textArea.setOpaque(false);
         scrollPane.setVisible(true);
+        
+        for (int i = 0; i < 4; i++) {
+            textAreas[i] = new JTextArea();
+            textAreas[i].setFont(font);
+            scrollPanes[i] = new JScrollPane(textAreas[i]);
+
+            // Set location and size for each JScrollPane
+            int x = (i < 4) ? 740 : 600;
+            int y = (i % 4) * 60 + 280;
+            scrollPanes[i].setBounds(x, y, 500, 40);
+
+            // Customize JScrollPane appearance
+            scrollPanes[i].setOpaque(false);
+            scrollPanes[i].getViewport().setOpaque(false);
+            scrollPanes[i].setBorder(null);
+            textAreas[i].setOpaque(false);
+
+            p1.add(scrollPanes[i]);
+        }
+
+        p1.setLayout(null);
         
         add(p1);
         setSize(1290, 750);
